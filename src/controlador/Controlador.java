0ed88fa03163c61f6f4e560a874354675ae37ebc
DanielCesurMalaga <daniel.fernandezcampos@cesurformacion.com>
2024-10-16 13:23:52 +0200

    creado nuevoProyecto hasta posicionar

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -5,27 +5,76 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import modelo.Movimiento;
 
 public class Controlador implements Initializable {
     // atributos
     @FXML
-    Circle circulo;
+    VBox escenarioPrincipal;
     @FXML
-    Pane principal;
+    Pane escenarioArriba;
+    @FXML
+    Pane escenarioAbajo;
+    @FXML
+    Label etiqueta1;
+    @FXML
+    Label etiqueta2;
+    @FXML
+    Label etiqueta3;
+    @FXML
+    Label etiqueta4;
+    @FXML
+    Label etiqueta5;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        circulo.setLayoutX(principal.getPrefWidth() / 2);
-        circulo.setLayoutY(principal.getPrefHeight() / 2);
-        circulo.setRadius(5);
-        circulo.setFill(Color.RED);
-
-        Movimiento movimiento = new Movimiento(circulo, principal, 30);
-        movimiento.start();
+        escenarioPrincipal.setStyle("-fx-background-color:darkgreen");
+        escenarioArriba.setStyle("-fx-background-color:grey");
+        escenarioAbajo.setStyle("-fx-background-color:white");
     }
 
+    public void etiquetaSeleccionada(MouseEvent evento) {
+        Label etiqueta = (Label) evento.getSource();
+        if (etiqueta1.getTextFill() == Color.RED) {
+            etiqueta1.setTextFill(Color.BLACK);
+        }
+        if (etiqueta2.getTextFill() == Color.RED) {
+            etiqueta2.setTextFill(Color.BLACK);
+        }
+        if (etiqueta3.getTextFill() == Color.RED) {
+            etiqueta3.setTextFill(Color.BLACK);
+        }
+        if (etiqueta4.getTextFill() == Color.RED) {
+            etiqueta4.setTextFill(Color.BLACK);
+        }
+        if (etiqueta5.getTextFill() == Color.RED) {
+            etiqueta5.setTextFill(Color.BLACK);
+        }
+        etiqueta.setTextFill(Color.RED);
+    }
+
+    public void posicionRaton(MouseEvent evento) {
+
+        if (etiqueta1.getTextFill() == Color.RED) {
+            etiqueta1.setText("Bola1 (" +
+                    (int) evento.getX() + "," + (int) evento.getY() + ")");
+        } else if (etiqueta2.getTextFill() == Color.RED) {
+            etiqueta2.setText("Bola2 (" +
+                    (int) evento.getX() + "," + (int) evento.getY() + ")");
+        } else if (etiqueta3.getTextFill() == Color.RED) {
+            etiqueta3.setText("Bola3 (" +
+                    (int) evento.getX() + "," + (int) evento.getY() + ")");
+        } else if (etiqueta4.getTextFill() == Color.RED) {
+            etiqueta4.setText("Bola4 (" +
+                    (int) evento.getX() + "," + (int) evento.getY() + ")");
+        } else if (etiqueta5.getTextFill() == Color.RED) {
+            etiqueta5.setText("Bola5 (" +
+                    (int) evento.getX() + "," + (int) evento.getY() + ")");
+        }
+
+    }
 }
